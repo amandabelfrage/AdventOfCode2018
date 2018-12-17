2567e81a216e17998c5e7e799f57779f5a5441fb
amandabelfrage <amanda.belfrage@gmail.com>
2018-12-18 00:04:46 +0100

    Update day3

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -7,66 +7,64 @@ public class Day3 {
 
     private static final String FILEPATH = "src/main/resources/Input_day3.txt";
 
-    public static void performDay3(){
+    public static void performDay3() {
         try {
             List<String> input = Helper.readWholeFile(FILEPATH);
-            List<String> results = createGrid(input);
-            System.out.println("The number of square inches claimed by two or more: " + results.get(0));
-            System.out.println("The id of the one that does not overlap: " +results.get(1));
-        }catch (IOException e){
+            List[][] grid = createGrid(input);
+            List<String> claimIds = updateGrid(grid, input);
+
+            int numberOfSquareInches = calculateSquareInches(grid, grid.length, grid[0].length);
+
+            List<String> overlaps = countTimesIdsAppear(grid, grid.length, grid[0].length);
+            String idNoOverlap = getIdNoOverlap(claimIds, overlaps);
+
+            System.out.println("The number of square inches claimed by two or more: " + numberOfSquareInches);
+            System.out.println("The id of the one that does not overlap: " + idNoOverlap);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    static List<String> createGrid(List<String> input){
-
-        List<String> returnValues = new ArrayList<>();
-        List<String> claimIds = new ArrayList<>();
+    static List[][] createGrid(List<String> input) {
 
         int maxX = Integer.MIN_VALUE;
         int maxY = Integer.MIN_VALUE;
 
-        for (String line : input){
-            String[] splittedLine = line.split(" ");
-            claimIds.add(splittedLine[0]);
-            int startXCoordinate = Integer.parseInt(splittedLine[2].split(",")[0]);
-            int startYCoordinate = Integer.parseInt((splittedLine[2].split(",")[1]).split(":")[0]);
-            String[] area = splittedLine[3].split("x");
+        for (String line : input) {
+            String[] splittedLine = line.split(" |\\,|x");
 
-            int xCorner = startXCoordinate + Integer.parseInt(area[0]) + 1;
-            int yCorner = startYCoordinate + Integer.parseInt(area[1]) + 1;
+            int startXCoordinate = Integer.parseInt(splittedLine[2]);
+            int startYCoordinate = Integer.parseInt((splittedLine[3]).split(":")[0]);
+            int xCorner = startXCoordinate + Integer.parseInt(splittedLine[4]) + 1;
+            int yCorner = startYCoordinate + Integer.parseInt(splittedLine[5]) + 1;
 
-            if(xCorner > maxX)
+            if (xCorner > maxX)
                 maxX = xCorner;
 
-            if(yCorner > maxY)
+            if (yCorner > maxY)
                 maxY = yCorner;
         }
+
         List[][] grid = new List[maxX][maxY];
-        updateGrid(grid, input);
-        returnValues.add(Integer.toString(calculateSquareInches(grid, maxX, maxY)));
-        List<String> overlaps = (countTimesIdsAppear(grid, maxX, maxY));
-        returnValues.add(getIdNoOverlap(claimIds, overlaps));
-        return returnValues;
+        return grid;
     }
 
-    static void updateGrid(List[][] grid, List<String> input){
-        for(String line : input){
-            String[] splittedLine = line.split(" ");
-            String[] coordinates = (splittedLine[2]).split(",");
-            coordinates[1] = coordinates[1].split(":")[0];
-            String[] size = (splittedLine[3]).split("x");
-            int startX = Integer.parseInt(coordinates[0]);
-            int startY = Integer.parseInt(coordinates[1]);
-            for(int y = startX; y <= startX + Integer.parseInt(size[0])-1; y++) {
-                for (int x = startY; x <= startY + Integer.parseInt(size[1])-1; x++) {
-                    if(grid[x][y] != null) {
+    static List<String> updateGrid(List[][] grid, List<String> input) {
+        List<String> claimIds = new ArrayList<>();
+
+        for (String line : input) {
+            String[] splittedLine = line.split(" |\\,|\\:|x");
+            claimIds.add(splittedLine[0]);
+            int startX = Integer.parseInt(splittedLine[2]);
+            int startY = Integer.parseInt(splittedLine[3]);
+            for (int y = startX; y <= startX + Integer.parseInt(splittedLine[5]) - 1; y++) {
+                for (int x = startY; x <= startY + Integer.parseInt(splittedLine[6]) - 1; x++) {
+                    if (grid[x][y] != null) {
                         List<String> updateValues = grid[x][y];
                         updateValues.add(splittedLine[0]);
                         grid[x][y] = updateValues;
-                    }
-                    else {
+                    } else {
                         List<String> newValue = new ArrayList<>();
                         newValue.add(splittedLine[0]);
                         grid[x][y] = newValue;
@@ -74,15 +72,16 @@ public class Day3 {
                 }
             }
         }
+        return claimIds;
     }
 
 
-    static int calculateSquareInches(List[][] grid, int xCorner, int yCorner){
+    static int calculateSquareInches(List[][] grid, int xCorner, int yCorner) {
         int numberOfSquareInches = 0;
-        for(int x = 0; x < xCorner; x++){
-            for(int y = 0; y < yCorner; y++){
+        for (int x = 0; x < xCorner; x++) {
+            for (int y = 0; y < yCorner; y++) {
                 List<String> values = grid[x][y];
-                if(values != null && values.size() > 1){
+                if (values != null && values.size() > 1) {
                     numberOfSquareInches += 1;
                 }
             }
@@ -90,16 +89,16 @@ public class Day3 {
         return numberOfSquareInches;
     }
 
-    static List<String> countTimesIdsAppear(List[][] grid, int xCorner, int yCorner){
+    static List<String> countTimesIdsAppear(List[][] grid, int xCorner, int yCorner) {
 
         List<String> overlaps = new ArrayList<>();
-        for(int x = 0; x < xCorner; x++) {
+        for (int x = 0; x < xCorner; x++) {
             for (int y = 0; y < yCorner; y++) {
                 List<String> coordValues = grid[x][y];
-                if(grid[x][y] != null) {
-                    for(String elem : coordValues) {
+                if (grid[x][y] != null) {
+                    for (String elem : coordValues) {
                         if (coordValues.size() > 1) {
-                            if (!overlaps.contains(elem)){
+                            if (!overlaps.contains(elem)) {
                                 overlaps.add(elem);
                             }
                         }
@@ -110,11 +109,11 @@ public class Day3 {
 
         return overlaps;
     }
-    
-    static String getIdNoOverlap(List<String> allIds, List<String> overlaps){
+
+    static String getIdNoOverlap(List<String> allIds, List<String> overlaps) {
         String id = "";
-        for (String elem : allIds){
-            if(!overlaps.contains(elem))
+        for (String elem : allIds) {
+            if (!overlaps.contains(elem))
                 id = elem;
         }
         return id;
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -11,20 +11,24 @@ public class Day3Test {
     public void TestNumberOfInches(){
         List<String> input = Arrays.asList("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2");
 
-        List<String> answer = Day3.createGrid(input);
-
-        Assert.assertEquals("4", answer.get(0));
+        List[][] answer = Day3.createGrid(input);
+        //x=8, y = 8
+        Assert.assertEquals(8, answer.length);
+        Assert.assertEquals(8, answer[0].length);
     }
+
 
     @Test
-    public void TestIdDoesNotOverlapSmall(){
+    public void TestClaimIds(){
         List<String> input = Arrays.asList("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2");
+        List[][] grid = Day3.createGrid(input);
 
-        List<String> answer = Day3.createGrid(input);
+        List<String> claimIds = Day3.updateGrid(grid, input);
 
-        Assert.assertEquals("#3", answer.get(1));
+        Assert.assertEquals(3, claimIds.size());
+        Assert.assertEquals("#2", claimIds.get(1));
     }
-
+/*
     @Test
     public void TestIdDoesNotOverlapLarge(){
         List<String> input = Arrays.asList("#1 @ 0,0: 5x5", "#2 @ 3,3: 3x3", "#3 @ 4,4: 4x4", "#4 @ 10,10: 2x2");
@@ -33,6 +37,7 @@ public class Day3Test {
 
         Assert.assertEquals("#4", answer.get(1));
     }
+*/
 
 
 
